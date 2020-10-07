6e3c10c2a9fae1fd2b1b51f8b2220169c183907c
Braian Villasanti <villasantibraian@icloud.com>
2020-10-07 16:53:52 -0300

    merged into single page

--- scripts.js
+++ scripts.js
@@ -12,3 +12,22 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+function enable_visibility(id) {
+  var e = document.getElementById(id);
+  console.log(e.style);
+  e.style.display = "block";
+  setTimeout(() => {e.style.opacity = 1;}, 100);
+}
+
+function disable_visibility(id) {
+  var e = document.getElementById(id);
+  e.style.opacity = 0;
+  setTimeout(() => {e.style.display = "none";}, 1000);
+}
+
+function change_color(id, bottom, padding){
+  var e = document.getElementById(id);
+  e.style.bottom = bottom;
+  document.getElementById("nav-bar").style.padding = padding;
+}
