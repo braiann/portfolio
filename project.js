b16d40e7adce1e255e12e3b718f53ba52c4938d6
Braian Villasanti <villasantibraian@icloud.com>
2021-01-21 03:08:50 -0300

    better responsivity

--- project.js
+++ project.js
@@ -4,7 +4,7 @@ function toggle_video_description()
 {
     const content = document.getElementById("content");
     const logo = document.getElementById("logo")
-    const mediaQuery = window.matchMedia("(min-width: 600px)");
+    const mediaQuery = window.matchMedia("(min-width: 758px)");
     if (mediaQuery.matches) {
         if (window.getComputedStyle(content).opacity == 1) {
             content.style.opacity = 0;
@@ -13,5 +13,10 @@ function toggle_video_description()
             content.style.opacity = 1;
             logo.style.opacity = 1;
         }
+    } else {
+        content.style.opacity = 1;
+        logo.style.opacity = 1;
     }
 }
+
+window.addEventListener('resize', () => toggle_video_description());
