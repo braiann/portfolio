3874f0b07e43248ee9fd83c4d6a06c81f2e0c5d0
Braian Villasanti <villasantibraian@icloud.com>
2021-01-21 16:57:31 -0300

    improved animation

--- index-script.js
+++ index-script.js
@@ -1,6 +1,8 @@
-let navbarButtons = document.getElementById("nav-bar").children;
-for (let i = 0; i < navbarButtons.length; i++) {
-    navbarButtons.item(i).style.opacity = 0;
-    setTimeout(() => navbarButtons.item(i).style.opacity = 1, i*100)
-}
+window.addEventListener('load', () => {
+    let navbarButtons = document.getElementById("nav-bar").children;
+    for (let i = 0; i < navbarButtons.length; i++) {
+        navbarButtons.item(i).style.opacity = 0;
+        setTimeout(() => navbarButtons.item(i).style.opacity = 1, i*100)
+    }
+});
 
