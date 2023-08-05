e39ac4270ef644166f66acc3e15032bebc74ae1f
Braian <villasantibraian@icloud.com>
2023-08-05 00:54:17 -0300

    fixed arrow dark mode colors & localization

--- js/localize.js
+++ js/localize.js
@@ -3,9 +3,18 @@ const enText = {
     "projects-navbar-option": "Projects",
     "contact-navbar-option": "Contact",
     "hi": "Hi!",
+    "hello1": "Hello.",
+    "hello2": "Hello.",
+    "hello3": "Hello.",
     "i-am-braian": "I'm <strong>Braian</strong>.",
     "im-from": "I'm from <strong>Argentina</strong>.",
     "i-am-a": "I'm a <strong id='developer'>developer</strong> <span style='display: none;' id='and'> and </span><strong id='designer'>designer</strong>",
+    "modi-desc": "Manage your amenities and save money.",
+    "allied-desc": "Website with easy access to the company's services.",
+    "blog-desc": "Blog with login for editing capabilities.",
+    "booktracker-desc": "Track the books you've read, and the progress in the ones you're reading.",
+    "world-desc": "Landing page. Example of what I can make.",
+    "password-desc": "Generate passwords with several parameters to choose from.",
     "interested-in-hiring": 'Interested in working with me? <a onclick="expandContactCard()" id="contact-me">Contact me</a>.',
     "change-language": "Change language"
 }
@@ -15,9 +24,18 @@ const esText = {
     "projects-navbar-option": "Proyectos",
     "contact-navbar-option": "Contacto",
     "hi": "¡Hola!",
+    "hello1": "Hola.",
+    "hello2": "Hola.",
+    "hello3": "Hola.",
     "i-am-braian": "Soy <strong>Braian</strong>.",
     "im-from": "Soy de <strong>Argentina</strong>.",
     "i-am-a": "Soy un <strong id='developer'>desarrollador</strong> <span style='display: none;' id='and'> y </span><strong id='designer'>diseñador</strong>",
+    "modi-desc": "Gestiona tus comodidades y ahorra dinero.",
+    "allied-desc": "Sitio web con acceso a los servicios de la empresa.",
+    "blog-desc": "Blog con autenticación para editar.",
+    "booktracker-desc": "Lleva rastro de los libros que lees y el progreso en los que estás leyendo.",
+    "world-desc": "Página de inicio. Ejemplo de lo que puedo hacer.",
+    "password-desc": "Genera contraseñas con varios parámetros para elegir.",
     "interested-in-hiring": '¿Te interesa trabajar conmigo? <a onclick="expandContactCard()" id="contact-me">Contactame</a>.',
     "change-language": "Cambiar idioma"
 }
@@ -39,9 +57,11 @@ function setTextToLanguage() {
     console.log(selectedLanguage)
     for (const [id, text] of Object.entries(selectedLanguage)) {
         if (id == "language") continue
+        console.log(id, text)
         const currentElement = document.getElementById(id)
-        console.log(text)
-        currentElement.innerHTML = text
+        if (currentElement) {
+            currentElement.innerHTML = text
+        }
     }
     console.log(`set to language ${selectedLanguage.language}`)
 }
